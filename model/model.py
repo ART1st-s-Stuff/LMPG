fef7d43688383885432cc6c9589b81bac5b0cc91
ARTI5T <15008780+ARTI5T@users.noreply.github.com>
2025-09-18 21:12:08 +0800

    Add build_dataset.py

--- model/model.py
+++ model/model.py
@@ -10,15 +10,15 @@ class HRNNEncoder(nn.Module):
         super().__init__()
         self.encoder = encoder
         self.ffnn = nn.Sequential(
-            nn.Linear(dim_output, dim_hidden),
+            nn.Linear(dim_hidden, dim_hidden),
             nn.GELU(),
             nn.Linear(dim_hidden, dim_output)
         )
         self._dim_output = dim_output
 
-    def forward(self, states, x):
-        states = self.encoder(states, x)
-        output = self.ffnn(torch.cat(states, dim=-1))
+    def forward(self, x, states):
+        states = self.encoder(x, states)
+        output = self.ffnn(states)
         return output, states
     
     @property
@@ -57,7 +57,7 @@ class HRNNEncoder(nn.Module):
                         **encoder_rnn_block_args
                     ),
                 ]
-            ), dim_hidden=dim_output_middle * 4, dim_output=512)
+            ), dim_hidden=dim_output_middle * 4, dim_output=1024)
     
 class HRNN(nn.Module):
     def __init__(self, encoders: List[HRNNEncoder], decoder: RNNBlock):
@@ -67,15 +67,22 @@ class HRNN(nn.Module):
         
     def forward(self, x: torch.Tensor, states=None, post_processing: Optional[Callable[[torch.Tensor], torch.Tensor]] = None):
         if states is None:
-            states = self.get_initial_states(x.device)
+            e_states, d_state = self.get_initial_states(x.device)
+        else:
+            e_states, d_state = states
         for t in range(x.size(1)):
-            x_t = x[:, t, :]
-            new_states = [self.encoders[i](states[i], x_t) for i in range(len(self.encoders))]
-            new_states_tensor = torch.cat([ns[1] for ns in new_states], dim=0)
-            y = self.decoder(new_states_tensor)
+            x_t = x[t, :]
+            encoded = [self.encoders[i](x_t, e_states[i]) for i in range(len(self.encoders))]
+            ns_list = []
+            y_list = []
+            for y, ns in encoded:
+                ns_list.append(ns)
+                y_list.append(y)
+            y_tensor = torch.cat(y_list, dim=0)
+            y = self.decoder(y_tensor, d_state)
             processed_y = post_processing(y) if post_processing is not None else y
             output = processed_y if t == 0 else torch.cat([output, processed_y], dim=1)
-            states = new_states
+            d_state = ns_list
         return output
     
     def self_regression(self, x: torch.Tensor, max_output: int, halt_if: Callable[[torch.Tensor], bool], states=None, post_processing: Optional[Callable[[torch.Tensor], torch.Tensor]] = None):
@@ -90,10 +97,10 @@ class HRNN(nn.Module):
 
     @staticmethod
     def default():
-        encoders = [ HRNNEncoder.default() for _ in range(16) ]
+        encoders = [ HRNNEncoder.default() for _ in range(8) ]
         decoder = RNNBlock(
             dim_input=encoders[-1].dim_output * len(encoders),
-            dim_hidden=2048,
+            dim_hidden=4096,
             dim_output=1024,
             num_layers=6
         )
@@ -104,4 +111,7 @@ class HRNN(nn.Module):
         return self.decoder.dim_output
     
     def get_initial_states(self, device: torch.device):
-        return [ [torch.zeros(1, encoder.dim_output, device=device) for _ in range(len(encoder.encoder.blocks))] for encoder in self.encoders ]
+        return (
+            [ encoder.encoder.get_initial_states(device) for encoder in self.encoders ],
+            self.decoder.get_initial_state().to(device)
+        )
--- model/rnn_module.py
+++ model/rnn_module.py
@@ -15,20 +15,21 @@ class RNNBlock(nn.Module):
             nn.Linear(dim_hidden, dim_output)
         )
         self.gate = nn.Sequential(
-            nn.Linear(dim_output, dim_hidden),
+            nn.Linear(dim_output + dim_input, dim_hidden),
             nn.Linear(dim_hidden, 1),
             nn.Tanh()
         )
         #self.initial_state = nn.Parameter(torch.zeros(1, 1, dim_output))
-        self.zeros = torch.zeros(1, 1, dim_output)
+        self.zeros = torch.zeros(dim_output)
         self._dim_output = dim_output
         
-    def forward(self, state, x):
-        next_state = self.ffnn(torch.cat([state, x], dim=-1))
-        gate = self.gate(next_state)
-        next_state = next_state * gate + state * (1 - gate)
+    def forward(self, x, state):
+        input = torch.cat([x, state], dim=-1)
+        current_state = self.ffnn(input)
+        gate = self.gate(input)
+        next_state = current_state * gate + state * (1 - gate)
         # Gate=0: keep old state, Gate=1: take new state
-        return next_state, gate
+        return next_state
     
     #def initialize(self, initial_state: torch.Tensor, batch_size: int):
     #    self.initial_state = nn.Parameter(initial_state.expand(1, batch_size, -1))
@@ -38,29 +39,35 @@ class RNNBlock(nn.Module):
     def dim_output(self):
         return self._dim_output
     
+    def get_initial_state(self):
+        return self.zeros
+    
 class StackedRNNBlock(nn.Module):
-    def __init__(self, blocks: List[RNNBlock], parallel: bool = False, residual: bool = True):
+    def __init__(self, blocks: List[RNNBlock]):
         super().__init__()
+        self._blocks = blocks
         self.blocks = nn.ModuleList(blocks)
-        self._dim_output = sum([b.dim_output for b in blocks])
-        self.parallel = parallel
-        self.residual = residual
+        self._dim_states = [b.dim_output for b in blocks]
+        self._dim_output = sum(self._dim_states)
+        self._state_slices = []
+        i = 0
+        for d in self._dim_states:
+            self._state_slices.append(slice(i, i + d))
+            i += d
 
-    def forward(self, state_list, x):
+    def forward(self, x, states):
         new_states = []
         input = x
-        for block, state in zip(self.blocks, state_list):
-            new_state, gate = block(state, input)
-            if self.residual:
-                new_state = new_state + state
+        for block, state_slice in zip(self.blocks, self._state_slices):
+            state = states[state_slice]
+            new_state = block(input, state)
             input = new_state
-            # Gate=0: keep old state, Gate=1: take new state
-            if self.parallel:
-                new_states.append(state * (1 - gate) + block.initial_state * gate)
-            else:
-                new_states.append(state * (1 - gate) + new_state * gate)
-        return new_states
+            new_states.append(new_state)
+        return torch.cat(new_states)
     
     @property
     def dim_output(self) -> int:
         return self._dim_output
+
+    def get_initial_states(self, device: torch.device):
+        return torch.cat([block.get_initial_state() for block in self._blocks]).to(device)
